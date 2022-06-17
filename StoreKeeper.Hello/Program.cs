44da6565ed799dab3e244379ed6032459a087ee1
Andrii Kurdiumov <kant2002@gmail.com>
2022-06-17 12:25:51 +0600

    Add sample for StoreKeeper

---
+++ StoreKeeper.Hello/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+// Make sure that add dotnet7 feed to nuget config, if you are not on latest Preview 6
+// which is downloaded after Jun 17 2022.
+
+// Build your DI container as usual.
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddScoped<ConsoleWrapper>();
+var provider = serviceCollection.BuildServiceProvider();
+
+// Use your service provider as usual.
+var wrapper = provider.GetRequiredService<ConsoleWrapper>();
+wrapper.WriteLine("Bye Bye Reflection!");
+
+class ConsoleWrapper
+{
+    public void WriteLine(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
